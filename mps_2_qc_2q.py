Fixed normalization, General work
I fixed the normalization to be general for the left canonical
normalization functions.  I also added a split tensor function
and a function to truncate an MPS down to bond dimension 2.
They seem to work.

mps_2_qc_2q.py
@@ -4,7 +4,7 @@ import scipy as sp
 
 def left_orthgonalize(mps):
     """
-    Takes an MPS of bond dimension 2 and left orthonalizes it.
+    Takes an MPS of bond dimension 2 and left orthogonalizes it.
 
     Parameters
     ----------
@@ -30,7 +30,41 @@ def left_orthgonalize(mps):
         mps[i] = q.reshape((2,2,2))
     temp = np.einsum('ia, aj', norm, mps[-1])
     q, norm = np.linalg.qr(temp)
-    mps[-1] = q/np.sqrt(2)
+    mps[-1] = q/np.linalg.norm(q)
+    return norm
+
+
+def left_orthgonalize_general(mps):
+    """
+    Takes an MPS and left orthogonalizes it.
+
+    Parameters
+    ----------
+    mps : A list of matrix product states of bond dimension 2.
+
+    Returns
+    -------
+    norm : The final normalization of the MPS stored in a
+           Hermitian matrix.
+
+    Notes
+    -----
+    Orthonalizes in place.
+    
+    """
+    shape = len(mps)
+    q, norm = np.linalg.qr(mps[0])
+    mps[0] = q
+    for i in range(1, shape-1):
+        ms = mps[i].shape
+        ns = norm.shape
+        temp = np.einsum('ia, ajk', norm,  mps[i]).reshape((ns[0]*ms[1], ms[2]))
+        q, norm = np.linalg.qr(temp)
+        qs = q.shape
+        mps[i] = q.reshape((ns[0],ms[1],qs[1]))
+    temp = np.einsum('ia, aj', norm, mps[-1])
+    q, norm = np.linalg.qr(temp)
+    mps[-1] = q/np.linalg.norm(q)
     return norm
 
     
@@ -165,96 +199,98 @@ def disentangle(mps, unis):
     return cap
     
         
-# if __name__ == '__main__':        
-L = 3
-mps_list = [np.random.normal(size=(2,2,2)) + 1j*np.random.normal(size=(2,2,2)) for i in range(L-2)]
-mps_list.insert(0, np.random.normal(size=(2,2)) + 1j*np.random.normal(size=(2,2)))
-mps_list.append(np.random.normal(size=(2,2)) + 1j*np.random.normal(size=(2,2)))
+if __name__ == '__main__':        
+    L = 3
+    mps_list = [np.random.normal(size=(2,2,2)) + 1j*np.random.normal(size=(2,2,2)) for i in range(L-2)]
+    mps_list.insert(0, np.random.normal(size=(2,2)) + 1j*np.random.normal(size=(2,2)))
+    mps_list.append(np.random.normal(size=(2,2)) + 1j*np.random.normal(size=(2,2)))
 
 
 
-# print(mps_list)
-left_orthgonalize(mps_list)
-# print(len(mps_list))
-# print(np.einsum('abi, abj', mps_list[1], mps_list[1]))
-U = mps_to_unitaries(mps_list)
-# print(len(U))
-# print([x.shape for x in U])
+    # print(mps_list)
+    left_orthgonalize(mps_list)
+    print(mps_list[-1].transpose().conjugate().dot(mps_list[-1]))
+    # exit()
+    # print(len(mps_list))
+    # print(np.einsum('abi, abj', mps_list[1], mps_list[1]))
+    U = mps_to_unitaries(mps_list)
+    # print(len(U))
+    # print([x.shape for x in U])
 
-vec = disentangle(mps_list, U)
-print(vec)
+    # vec = disentangle(mps_list, U)
+    # print(vec)
 
-# print(mps_list[1][j,k,l])
-# print(U[1][j,k,0,l])
+    # print(mps_list[1][j,k,l])
+    # print(U[1][j,k,0,l])
 
-# print(np.einsum('abk, abij', mps_list[1], U[1]))
-# print(np.einsum('ai, aj', mps_list[0], U[0]))
-# print(np.einsum('ab, abij', mps_list[2], U[2]))
+    # print(np.einsum('abk, abij', mps_list[1], U[1]))
+    # print(np.einsum('ai, aj', mps_list[0], U[0]))
+    # print(np.einsum('ab, abij', mps_list[2], U[2]))
 
-# print([x.shape for x in mps_list])
-# print([x.shape for x in U])
+    # print([x.shape for x in mps_list])
+    # print([x.shape for x in U])
 
-# check = np.einsum('ab, bfc, cd, ag, gfke, edji', mps_list[0],
-#                   mps_list[1],
-#                   mps_list[2],
-#                   U[0],
-#                   U[1],
-#                   U[2])
-# check = np.einsum('ab, bc, cd, jida', mps_list[0],
-#                   mps_list[1],
-#                   U[1],
-#                   U[0])
-# print(np.einsum('ab, jiba', mps_list[0], U[0]))
-# print(check)
+    # check = np.einsum('ab, bfc, cd, ag, gfke, edji', mps_list[0],
+    #                   mps_list[1],
+    #                   mps_list[2],
+    #                   U[0],
+    #                   U[1],
+    #                   U[2])
+    # check = np.einsum('ab, bc, cd, jida', mps_list[0],
+    #                   mps_list[1],
+    #                   U[1],
+    #                   U[0])
+    # print(np.einsum('ab, jiba', mps_list[0], U[0]))
+    # print(check)
 
-# print(mps_list[-1].dot(U[-1]))
-          
-
-# Umat = build_circuit(U)
-# print(Umat.dot(Umat.conjugate().transpose()))
-# print(Umat.transpose().conjugate().dot(Umat))
-
-# # # print([G_norm(x) for x in U[1:]])
-
-# wf = build_wavefunction(mps_list)
-# Umat.transpose().conjugate().dot(wf)
-# print(Umat.dot(wf.transpose()))
-# print(wf)
-# print(wf.dot(wf.transpose()))
-# wf_orig = np.einsum('ia,ajb,bk', mps_list[0], mps_list[1], mps_list[2])
-# print(wf_orig.reshape((8,1)))
+    # print(mps_list[-1].dot(U[-1]))
 
 
-        
+    Umat = build_circuit(U)
+    print(Umat.dot(Umat.conjugate().transpose()))
+    print(Umat.transpose().conjugate().dot(Umat))
 
-# print(mps_list[0].dot(mps_list[0].conjugate().transpose()))
-# print(mps_list)
+    # # print([G_norm(x) for x in U[1:]])
 
-# A = mps_list[1].reshape((4,2))
-# print(A.conjugate().transpose().dot(A))
-# X = sp.linalg.null_space(A.conjugate().transpose())
-# print(X)
-# G = np.hstack((A, X)).reshape((2,2,2,2))
-# # G = np.zeros((2,2,2,2))
-# # G[0,:,:,:] = mps_list[1]
-# # G[1,:,:,:] = X.reshape((2,2,2))
-# # G = G.transpose((0,3,1,2))
-# print(np.einsum('ikab, jlab', G, G.conjugate()))
-# print(np.outer(np.eye(2), np.eye(2)).reshape((2,2,2,2)))
+    wf = build_wavefunction(mps_list)
+    Umat.transpose().conjugate().dot(wf)
+    # print(Umat.dot(wf.transpose()))
+    # print(wf)
+    # print(wf.dot(wf.transpose()))
+    # wf_orig = np.einsum('ia,ajb,bk', mps_list[0], mps_list[1], mps_list[2])
+    # print(wf_orig.reshape((8,1)))
 
-# A = mps_list[-1].reshape((4,1))
-# print(A.conjugate().transpose().dot(A))
-# X = sp.linalg.null_space(A.conjugate().transpose())
-# print(X)
-# # print(X.transpose().dot(X))
-# # G = np.zeros((2,2,2,2))
-# G = np.hstack((A, X)).reshape((2,2,2,2))
 
-# # G[0,0,:,:] = A
-# # G[0,1,:,:] = X[:,0].reshape((2,2))
-# # G[1,0,:,:] = X[:,1].reshape((2,2))
-# # G[1,1,:,:] = X[:,2].reshape((2,2))
-# # # G = G.transpose((0,3,1,2))
-# print(np.einsum('ikab, jlab', G, G.conjugate()))
-# print(np.outer(np.eye(2), np.eye(2)).reshape((2,2,2,2)))
+
+
+    # print(mps_list[0].dot(mps_list[0].conjugate().transpose()))
+    # print(mps_list)
+
+    # A = mps_list[1].reshape((4,2))
+    # print(A.conjugate().transpose().dot(A))
+    # X = sp.linalg.null_space(A.conjugate().transpose())
+    # print(X)
+    # G = np.hstack((A, X)).reshape((2,2,2,2))
+    # # G = np.zeros((2,2,2,2))
+    # # G[0,:,:,:] = mps_list[1]
+    # # G[1,:,:,:] = X.reshape((2,2,2))
+    # # G = G.transpose((0,3,1,2))
+    # print(np.einsum('ikab, jlab', G, G.conjugate()))
+    # print(np.outer(np.eye(2), np.eye(2)).reshape((2,2,2,2)))
+
+    # A = mps_list[-1].reshape((4,1))
+    # print(A.conjugate().transpose().dot(A))
+    # X = sp.linalg.null_space(A.conjugate().transpose())
+    # print(X)
+    # # print(X.transpose().dot(X))
+    # # G = np.zeros((2,2,2,2))
+    # G = np.hstack((A, X)).reshape((2,2,2,2))
+
+    # # G[0,0,:,:] = A
+    # # G[0,1,:,:] = X[:,0].reshape((2,2))
+    # # G[1,0,:,:] = X[:,1].reshape((2,2))
+    # # G[1,1,:,:] = X[:,2].reshape((2,2))
+    # # # G = G.transpose((0,3,1,2))
+    # print(np.einsum('ikab, jlab', G, G.conjugate()))
+    # print(np.outer(np.eye(2), np.eye(2)).reshape((2,2,2,2)))
 

mps_2_qc_nq.py
@@ -3,12 +3,61 @@ import scipy as sp
 import mps_2_qc_2q as m2q
 
 
+def split_tensor(left, right, dbond=2, where='mid'):
+    if where == 'mid':
+        ls = left.shape
+        rs = right.shape
+        mat = np.einsum('ija, akl', left, right).reshape((ls[0]*ls[1], rs[1]*rs[2]))
+        U, s, Vd = np.linalg.svd(mat, full_matrices=False)
+        sl = np.diag(np.sqrt(s))
+        Al = U.dot(sl[:, :dbond]).reshape((ls[0], ls[1], dbond))
+        Ar = sl[:dbond, :].dot(Vd).reshape((dbond, rs[1], rs[2]))
+        return (Al, Ar)
+    elif where == 'start':
+        ls = left.shape
+        rs = right.shape
+        mat = np.einsum('ia, ajk', left, right).reshape((ls[0], rs[1]*rs[2]))
+        U, s, Vd = np.linalg.svd(mat, full_matrices=False)
+        sl = np.diag(np.sqrt(s))
+        Al = U.dot(sl[:, :dbond]).reshape((ls[0], dbond))
+        Ar = sl[:dbond, :].dot(Vd).reshape((dbond, rs[1], rs[2]))
+        return (Al, Ar)
+    elif where == 'end':
+        ls = left.shape
+        rs = right.shape
+        mat = np.einsum('ija, ak', left, right).reshape((ls[0]*ls[1], rs[1]))
+        U, s, Vd = np.linalg.svd(mat, full_matrices=False)
+        sl = np.diag(np.sqrt(s))
+        Al = U.dot(sl[:, :dbond]).reshape((ls[0], ls[1], dbond))
+        Ar = sl[:dbond, :].dot(Vd).reshape((dbond, rs[1]))
+        return (Al, Ar)
+    else:
+        raise KeyError("where argument must be 'mid', 'start', or 'end'")
+
+    
+def truncate_mps(mps):
+    trunc_mps = list()
+    current, coming = split_tensor(mps[0], mps[1], where='start')
+    trunc_mps.append(current)
+    for i in range(2, len(mps)-1):
+        current, coming = split_tensor(coming, mps[i])
+        trunc_mps.append(current)
+    current, coming = split_tensor(coming, mps[-1], where='end')
+    trunc_mps.append(current)
+    trunc_mps.append(coming)
+    return trunc_mps
+
 
 if __name__ == '__main__':
     L = 3
     mps_list = [np.random.normal(size=(3,2,3)) for i in range(L-2)]
     mps_list.insert(0, np.random.normal(size=(2,3)))
-    mps_list.append(np.random.normal(size=(2,3)))
+    mps_list.append(np.random.normal(size=(3,2)))
 
-    
+    m2q.left_orthgonalize_general(mps_list)
+    # print(mps_list[0].transpose().dot(mps_list[0]))
+
+    trunc_mps = truncate_mps(mps_list)
+    print([x.shape for x in trunc_mps])
+    m2q.left_orthgonalize(trunc_mps)
 